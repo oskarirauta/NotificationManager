6a60413a72a047bdb157ae3aca91332e2de02553
Oskari Rauta <oskari.rauta@gmail.com>
2019-02-24 14:54:13 +0200

    Update tokenList.php

--- templates/tokenList.php
+++ templates/tokenList.php
@@ -7,16 +7,33 @@ require_once $ROOT_DIR . '/inc/db.php';
 
 function tokenList() {
 
-  global $page, $db, $session, $SCRIPT_FILE;
+  global $page, $db, $session, $ROOT_DIR, $SCRIPT_FILE;
+
+  $certError = false;
+  $certFile = 'certs/' . $session['product'] . ( $session['debugOnly'] ? '_debug' : '') . '.pem';
 
   $ret = "<div id='tokenlist'>\r\n";
 
+  if ( !file_exists($ROOT_DIR . '/' . $certFile )) {
+    $certError = true;
+    $ret .= "<div style='text-align: center;'>\r\n";
+    $ret .= "<div class='error'>\r\n";
+    $ret .= "ERROR: Certificate <i>" . $certFile . "</i> is missing.\r\n";
+    $ret .= "</div>\r\n";
+    $ret .= "</div>\r\n";
+  }
+
+  $ret .= "<table class='controlPanel' cellspacing='0' cellpadding='0' border='0'>\r\n";
+
   if ( !empty($session['targetId'])) {
 
-    $ret .= "<div class='pagelist'>";
-    $ret .= "<a href='./" . $SCRIPT_FILE . "?page=main&uuid=" . $session['uuid'] . "&debugOnly=" . ( $session['debugOnly'] ? "1" : "0" ) . "'>Exit single token mode</a>";
-    $ret .= "<a style='margin-left: 16px;' href='./" . $SCRIPT_FILE . "?page=main&uuid=" . $session['uuid'] . "&debugOnly=" . ( $session['debugOnly'] ? "1" : "0" ) . "&deleteToken=1&targetId=" . $session['targetId'] . "'>Remove this token</a>";
-    $ret .= "</div>";
+    $ret .= "<tr class='pageList'>\r\n";
+    $ret .= "<td colspan='2'>\r\n";
+    $ret .= "<a href='./" . $SCRIPT_FILE . "?page=main&uuid=" . $session['uuid'] . "&debugOnly=" . ( $session['debugOnly'] ? "1" : "0" ) . "'>Filter off</a>\r\n";
+    $ret .= "</td><td colspan='2'>\r\n";
+    $ret .= "<a style='margin-left: 16px;' href='./" . $SCRIPT_FILE . "?page=main&uuid=" . $session['uuid'] . "&debugOnly=" .( $session['debugOnly'] ? "1" : "0" ) . "&deleteToken=1&targetId=" . $session['targetId'] . "'>Remove this</a>\r\n";
+    $ret .= "</td><td>&nbsp;</td>\r\n";
+    $ret .= "</tr>\r\n";
 
   } else {
 
@@ -25,11 +42,12 @@ function tokenList() {
     if ( $pages == 0 )
       $pages = 1;
 
-    $ret .= "<div class='pagelist'>\r\n";
-    $ret .= "<form id='paginator' action='" . $SCRIPT_FILE . "' method='post'>\r\n";
+    $ret .= "<form id='paginator' name='paginator' action='" . $SCRIPT_FILE . "' method='post'>\r\n";
     $ret .= inputType('hidden', 'uuid', $session['uuid']) . "\r\n";
     $ret .= inputType('hidden', 'debugOnly', $session['debugOnly'] ? '1' : '0') . "\r\n";
-    $ret .= "Page: <select name='pageIndex' onchange='this.form.submit()'>\r\n";
+
+    $ret .= "<tr class='pageList'><td>\r\n";
+    $ret .="Page: </td><td colspan='3'><select name='pageIndex' onchange='this.form.submit()'>\r\n";
 
     $pageNo = 1;
     $thisPage = empty($session['pageIndex']) ? 1 : intval($session['pageIndex']);
@@ -40,26 +58,52 @@ function tokenList() {
     }
 
     $ret .= "</select>\r\n";
+    $ret .= "</td><td>&nbsp;</td></tr>\r\n";
     $ret .= "</form>\r\n";
-    $ret .= "</div>\r\n";
   }
 
-  $ret .= "<div class='messagebox'>\r\n";
-  $ret .= "<form id='paginator' action='" . $SCRIPT_FILE . "' method='post'>\r\n";
+  $ret .= "<tr><td colspan='5'><hr/></td></tr>\r\n";
+
+  $ret .= "<form id='messagebox' name='messagebox' action='" . $SCRIPT_FILE . "' method='post'>\r\n";
   $ret .= inputType('hidden', 'uuid', $session['uuid']) . "\r\n";
   $ret .= inputType('hidden', 'debugOnly', $session['debugOnly'] ? '1' : '0') . "\r\n";
   $ret .= inputType('hidden', 'page', 'send') . "\r\n";
   if ( !empty($session['targetId']))
     $ret .= inputType('hidden', 'targetId', $session['targetId']) . "\r\n";
-  $ret .= "Message: " . inputType('text', 'payload', '', [ 'placeholder' => 'message', 'class' => 'messageField', 'required' => true ]) . "\r\n";
-  $ret .= inputType('submit', 'send', 'send', [ 'class' => 'sendBtn' ]) . "\r\n";
-  $ret .= "</form>\r\n";
-  $ret .= "</div>\r\n";
+  $ret .= "<tr class='radio'>\r\n";
+  $ret .= "<td class='label'><label for='sound'>Sound:</label></td>\r\n";
+  $ret .= "<td class='radiobtn' colspan='4'><input type='radio' name='sound' value='default'>&nbsp;Default</input>\r\n";
+  $ret .= "</tr><tr>\r\n";
+  $ret .= "<tr class='radio'><td class='label'>&nbsp;</td><td class='radiobtn' colspan='4'><input type='radio' name='sound' value=''>&nbsp;None</input>\r\n";
+  $ret .= "</td></tr>\r\n";
+  $ret .= "<tr class='radio'><td class='label'>&nbsp;</td><td class='radiobtn'><input type='radio' name='sound' value='custom'>&nbsp;Custom</input>\r\n";
+  $ret .= "</td><td class='field' colspan='2'>\r\n";
+  $ret .= inputType('text', 'customSound', '', [ 'placeholder' => 'custom sound', 'class' => 'messageField', 'autocomplete' => 'off' ]);
+  $ret .= "</td><td>&nbsp;</td></tr>\r\n";
+  $ret .= "<tr class='inputField'><td class='label'>\r\n";
+  $ret .= "Badge:\r\n";
+  $ret .= "</td><td class='field'>\r\n";
+  $ret .= inputType('text', 'badge', '', [ 'placeholder' => 'badge number', 'class' => 'messageField', 'autocomplete' => 'off' ]);
+  $ret .= "</td><td colspan='3'>&nbsp;</td></tr>";
+  $ret .= "<tr class='inputField'><td class='label'>\r\n";
+  $ret .= "Message:\r\n";
+  $ret .= "</td><td class='field'>\r\n";
+  $ret .= inputType('text', 'payload', '', [ 'placeholder' => 'message', 'class' => 'messageField', 'autocomplete' => 'off', 'required' => true ]) . "\r\n";
+  $ret .= "</td><td>&nbsp;</td><td class='submitbtn'>\r\n";
 
   $tokens = [];
   if ( empty($session['targetId']))
     $tokens = $db -> getTokens($session['product'], $session['debugOnly'] ? 1 : 0, $thisPage - 1);
   else $tokens = $db -> getToken($session['targetId']);
+
+  if ( count($tokens) == 0 || $certError )
+    $ret .= inputType('submit', 'send', 'send', [ 'class' => 'sendBtn', 'disabled' => true ]) . "\r\n";
+  else
+    $ret .= inputType('submit', 'send', 'send', [ 'class' => 'sendBtn' ]) . "\r\n";
+
+  $ret .= "</td></tr>\r\n";
+  $ret .= "</form>\r\n";
+  $ret .= "</table>\r\n";
 
   $ret .= "<div class='tokenContainer'>\r\n";
 
