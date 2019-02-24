1908244f7b2f68220531ad654bb3ff85cf45bca7
Oskari Rauta <oskari.rauta@gmail.com>
2019-02-24 14:52:52 +0200

    Update login.php
    Updated login page

--- templates/login.php
+++ templates/login.php
@@ -4,17 +4,18 @@ global $ROOT_DIR;
 
 require_once $ROOT_DIR . '/inc/generic.php';
 require_once $ROOT_DIR . '/inc/db.php';
+require_once $ROOT_DIR . '/inc/settings.php';
 
 function generate_page() {
 
-  global $session, $page, $SCRIPT_FILE;
+  global $session, $page, $SCRIPT_FILE, $products;
   if ( !$session['ssl'] ) {
     $page = notSSL();
     return;
   }
 
   $page['title'] = empty($session['error']) ? 'Login' : 'Error';
-  $page['body'] .= "<FORM ID='login' ACTION='" . $SCRIPT_FILE . "' METHOD='POST'>\r\n";
+  $page['body'] .= "<form id='login' name='loginform' action='" . $SCRIPT_FILE . "' method='post'>\r\n";
 
   $page['body'] .= "<div class='container'>\r\n";
 
@@ -42,7 +43,18 @@ function generate_page() {
 
   $page['body'] .= "<div class='row'>\r\n";
   $page['body'] .= "<div class='label'>Product:</div>\r\n";
+  $page['body'] .= "<div class='field'>\r\n";
+  $page['body'] .= "<select name='product' class='loginField' style='width: 140px;' required>\r\n";
+
+  foreach ( array_keys($products) as $product ) {
+    $page['body'] .= "  <option value='" . strtolower($product) . "'>" . $product . "</option>\r\n";
+  }
+
+  $page['body'] .= "</select>\r\n";
+  $page['body'] .= "</div>\r\n";
+/*
   $page['body'] .= "<div class='field'>" . inputType('text', 'product', '', [ 'placeholder' => 'product code', 'class' => 'loginField', 'required' => true ]) . "</div>\r\n";
+*/
   $page['body'] .= "<div class='req'>&nbsp;*</div>\r\n";
   $page['body'] .= "</div>\r\n";
 
